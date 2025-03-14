cb974e6cbce834f3aab3b8228bbf36d890bc1465
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-03-14 19:28:08 -0300

    Update Angular Material

--- filter-list-user/src/app/components/components.module.ts
+++ filter-list-user/src/app/components/components.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from "@angular/core";
-import { AngularMaterialModule } from "../angular-material/angular-material.module";
+import { NgModule } from '@angular/core';
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from '@angular/forms';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
-    declarations: [],
-    imports: [
-        AngularMaterialModule,
-    ],
-    exports: []
+  declarations: [UserDetailsComponent, FilterComponent, UsersListComponent],
+  imports: [FormsModule, AngularMaterialModule],
+  exports: [UserDetailsComponent, FilterComponent, UsersListComponent],
 })
 export class ComponentsModule {}
