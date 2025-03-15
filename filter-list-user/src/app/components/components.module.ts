20c4a79042cd3ced8bf91864d2b80ba5e6fdd6bc
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-03-15 17:54:35 -0300

    Update pipes

--- filter-list-user/src/app/components/components.module.ts
+++ filter-list-user/src/app/components/components.module.ts
@@ -4,10 +4,12 @@ import { UserDetailsComponent } from './user-details/user-details.component';
 import { FilterComponent } from './filter/filter.component';
 import { FormsModule } from '@angular/forms';
 import { UsersListComponent } from './users-list/users-list.component';
+import { PipesMoudle } from '../pipes/pipes.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [UserDetailsComponent, FilterComponent, UsersListComponent],
-  imports: [FormsModule, AngularMaterialModule],
+  imports: [FormsModule, AngularMaterialModule, PipesMoudle, CommonModule],
   exports: [UserDetailsComponent, FilterComponent, UsersListComponent],
 })
 export class ComponentsModule {}
